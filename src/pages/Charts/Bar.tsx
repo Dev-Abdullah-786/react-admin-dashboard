5a328c4410f8d840214d515433916348032dc3ec
Muhammad Abdullah Khan <abdullahkhanlodhi1458@gmail.com>
2026-07-19 10:06:07 +0500

    add Bar page with Olympic medal counts chart

---
+++ src/pages/Charts/Bar.tsx
@@ -0,0 +1,50 @@
+import {
+  barCustomSeries,
+  barPrimaryXAxis,
+  barPrimaryYAxis,
+} from "../../data/dummy";
+import {
+  Inject,
+  Legend,
+  Tooltip,
+  Category,
+  DataLabel,
+  ColumnSeries,
+  ChartComponent,
+  SeriesDirective,
+  SeriesCollectionDirective,
+} from "@syncfusion/ej2-react-charts";
+import ChartsHeader from "../../components/ChartsHeader";
+import { useStateContext } from "../../contexts/useStateContext";
+
+const Bar = () => {
+  const { currentMode } = useStateContext();
+
+  return (
+    <div className="m-4 md:m-10 mt-24 p-10 bg-white dark:bg-secondary-dark-bg rounded-3xl">
+      <ChartsHeader category="Bar" title="Olympic Medal Counts - RIO" />
+      <div className=" w-full">
+        <ChartComponent
+          id="charts"
+          primaryXAxis={barPrimaryXAxis}
+          primaryYAxis={barPrimaryYAxis}
+          chartArea={{ border: { width: 0 } }}
+          tooltip={{ enable: true }}
+          background={currentMode === "Dark" ? "#33373E" : "#fff"}
+          legendSettings={{ background: "white" }}
+        >
+          <Inject
+            services={[ColumnSeries, Legend, Tooltip, Category, DataLabel]}
+          />
+          <SeriesCollectionDirective>
+            {barCustomSeries.map((item, index) => (
+              <SeriesDirective key={index} {...item} />
+            ))}
+          </SeriesCollectionDirective>
+        </ChartComponent>
+      </div>
+    </div>
+  );
+};
+
+export default Bar;
